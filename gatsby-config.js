6a0559a763162707e3ee00035cb32b1782e78297
Xuffasch <tristan.lucas@gmail.com>
2021-08-11 18:11:53 +0200

    Initial commit - Add index and about pages

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteURL: "www.localhost.com",
+    title: "FrontendMasters - Intro to Gatsby",
+    description: 'Intro site',
+    image: "https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg"
+  }
+}
